4e8dd14073b20a8b4c901b619d7964dcc116e97a
Jinseo Park <jinseo@google.com>
2020-10-18 13:12:32 +0200

    F

--- src/data/stores.js
+++ src/data/stores.js
@@ -3,19 +3,19 @@ export const stores = [{
   description: 'Buzz furniture and home accessories are practical, well designed and affordable.',
   votes: 323,
 }, {
-  name: 'Buzz Furniture',
-  description: 'Buzz furniture and home accessories are practical, well designed and affordable.',
-  votes: 278,
-}, {
-  name: 'Buzz Furniture',
+  name: 'Maisons du Monde',
   description: 'Buzz furniture and home accessories are practical, well designed and affordable.',
   votes: 151,
 }, {
-  name: 'Buzz Furniture',
+  name: 'IKIA Furniture',
   description: 'Buzz furniture and home accessories are practical, well designed and affordable.',
   votes: 189,
 }, {
-  name: 'Buzz Furniture',
+  name: 'Yellow Jackets Furniture',
+  description: 'Yellow Jackets furniture and home accessories are practical, well designed and affordable.',
+  votes: 278,
+}, {
+  name: 'Home Furniture',
   description: 'Buzz furniture and home accessories are practical, well designed and affordable.',
   votes: 398,
 }];
--- src/data/weeks.js
+++ src/data/weeks.js
@@ -1,5 +1,7 @@
 import { stores } from './stores';
 import imageFurnitureCover from './images/themes/furniture_cover.jpg';
+import imageFurniture2Cover from './images/themes/furniture2_cover.jpg';
+import imageFoodCover from './images/themes/food_cover.jpg';
 
 export const weeks = [{
   theme: 'Furniture Week',
@@ -9,12 +11,12 @@ export const weeks = [{
 }, {
   theme: 'Street Food Week',
   dates: 'Oct. 26 - Nov. 1',
-  image: imageFurnitureCover,
+  image: imageFoodCover,
   stores: stores.slice(0, 5),
 }, {
-  theme: 'Wearable Week',
+  theme: 'Furniture Week',
   dates: 'Nov. 2 - 8',
-  image: imageFurnitureCover,
+  image: imageFurniture2Cover,
   stores: stores.slice(0, 5),
   voting: true,
   voted: false,
